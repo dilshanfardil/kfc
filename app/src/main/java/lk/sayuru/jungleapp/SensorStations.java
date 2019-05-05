b43df284ad719e109f1d2bf74a767bf92101e1e4
info@avalanche.lk <avalanche@bitbucket>
2019-05-05 12:46:52 +0530

    Sync with the db the ui

---
+++ app/src/main/java/lk/sayuru/jungleapp/SensorStations.java
@@ -0,0 +1,15 @@
+package lk.sayuru.jungleapp;
+
+/*
+ *
+ * Project Name : ${PROJECT}
+ * Created by Janiru on 5/5/2019 12:32 PM.
+ *
+ */public class SensorDTO {
+
+     private double humidity;
+     private double temparature;
+     private double humidity;
+     private double humidity;
+
+}
